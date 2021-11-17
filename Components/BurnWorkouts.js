ddc5fd28687255ec3fa53d1294e5134467546db5
Amar Omerika <amar.omerika@edu.fit.ba>
2021-11-17 21:30:01 +0100

    added flatlist designed, designed burnworkout card

--- Components/BurnWorkouts.js
+++ Components/BurnWorkouts.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
 	StyleSheet,
 	Text,
@@ -6,21 +6,68 @@ import {
 	Image,
 	TouchableOpacity,
 	TextInput,
-	KeyboardAvoidingView,
-	ScrollView,
+	FlatList,
 } from "react-native";
-
+import proba from "../assets/proba.png";
 export default function BurnWorkouts() {
+	const [data, setData] = useState([]);
+
+	const getData = async () => {
+		const res = await fetch(
+			"https://run.mocky.io/v3/c8c75a8c-bd80-41b0-85f6-6097d0cb235f"
+		);
+		const answer = await res.json();
+		setData(answer);
+	};
+	useEffect(() => {
+		getData();
+	}, []);
 	return (
 		<View style={styles.container}>
-			<Text>Burnnnn Groups</Text>
+			<Text style={styles.text}>Basic FlatList Example</Text>
+			<FlatList
+				data={data}
+				keyExtractor={(item) => item.id}
+				renderItem={({ item }) => (
+					<View
+						style={{ flexDirection: "row", marginBottom: 20, marginTop: 10 }}
+					>
+						<View>
+							<Image source={item.Image} style={styles.img} />
+						</View>
+						<View style={styles.description}>
+							<Text style={styles.descriptionText}>{item.title}</Text>
+						</View>
+					</View>
+				)}
+			/>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		alignSelf: "center",
+		flex: 1,
 		marginTop: 50,
+		marginLeft: 5,
+	},
+	img: {
+		width: 133,
+		height: 83,
+		borderRadius: 10,
+	},
+	description: {
+		backgroundColor: "#C0C0C0",
+		height: 85,
+		flex: 1,
+		borderRadius: 10,
+		marginLeft: 4,
+		marginRight: 5,
+	},
+	descriptionText: {
+		flex: 1,
+		marginLeft: 4,
+		marginRight: 4,
+		marginTop: 3,
 	},
 });
