5456ec2b59f577c89cbb76ba95bcd16393936f42
Amar Omerika <amar.omerika@edu.fit.ba>
2021-11-22 18:05:54 +0100

    added actions in redux store

--- Store/user.js
+++ Store/user.js
@@ -1,30 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-	value: 0,
+	user: null,
 };
 
 export const userSlice = createSlice({
 	name: "user",
 	initialState,
 	reducers: {
-		increment: (state) => {
+		login: (state, action) => {
 			// Redux Toolkit allows us to write "mutating" logic in reducers. It
 			// doesn't actually mutate the state because it uses the Immer library,
 			// which detects changes to a "draft state" and produces a brand new
 			// immutable state based off those changes
-			state.value += 1;
+			state.user = action.payload;
 		},
-		decrement: (state) => {
-			state.value -= 1;
-		},
-		incrementByAmount: (state, action) => {
-			state.value += action.payload;
+		logout: (state) => {
+			state.user = null;
 		},
 	},
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = userSlice.actions;
+export const { login, logout } = userSlice.actions;
 
 export default userSlice.reducer;
+
+export const Login = (fullUserObj) => async (dispatch) => {
+	try {
+		dispatch(login(fullUserObj));
+	} catch (e) {
+		return console.error(e.message);
+	}
+};
+export const Logout = () => async (dispatch) => {
+	try {
+		dispatch(logout());
+	} catch (e) {
+		return console.error(e.message);
+	}
+};
