13da5038bef6af4edcead60ca9fb41ba994c6fbc
Amar Omerika <amar.omerika@edu.fit.ba>
2021-11-22 17:27:51 +0100

    set up redux store

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import RouteStack from "./routes/routeStack";
+import { store } from "./Store/store";
+import { Provider } from "react-redux";
+
 export default function App() {
-	return <RouteStack />;
+	return (
+		<Provider store={store}>
+			<RouteStack />
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({
