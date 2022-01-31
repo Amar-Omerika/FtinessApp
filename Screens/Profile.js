9bd914160770ce6255d2d30b56b127e02df9d269
Amar Omerika <amar.omerika@edu.fit.ba>
2022-01-31 18:05:46 +0100

    finished navbar in profile screen

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -7,11 +7,14 @@ import {
 	TouchableOpacity,
 	TextInput,
 	KeyboardAvoidingView,
+	KeyboardAwareScrollView,
 	ScrollView,
 } from "react-native";
 import { useDispatch } from "react-redux";
 import { logout } from "../Store/user";
 import { useNavigation } from "@react-navigation/core";
+import mema from "../assets/mema.png";
+import edit from "../assets/edit.png";
 export default function Profile() {
 	const navigation = useNavigation();
 	const dispatch = useDispatch();
@@ -21,34 +24,75 @@ export default function Profile() {
 	};
 	return (
 		<View style={styles.container}>
-			<View style={{ alignSelf: "center", marginTop: 500 }}>
-				<TouchableOpacity onPress={() => handleLogout()}>
-					<Text style={{ color: "white" }}>Logout</Text>
-				</TouchableOpacity>
-				<TouchableOpacity
-					style={{
-						backgroundColor: "#4267B2",
-						marginTop: 30,
-						alignItems: "center",
-						height: 50,
-						width: 229,
-						borderRadius: 50,
-						alignSelf: "center",
-					}}
-					onPress={() => navigation.navigate("Login")}
-				>
+			<ScrollView style={{ flex: 1 }}>
+				<View style={{ flexDirection: "row" }}>
+					<View>
+						<Image
+							source={mema}
+							style={{ width: 80, height: 80, marginTop: 40, marginLeft: 20 }}
+						/>
+					</View>
+					<View style={{ marginTop: 50, marginLeft: 30 }}>
+						<Text style={{ color: "white", fontSize: 30 }}>Hi,</Text>
+						<Text style={{ color: "white", fontSize: 20 }}>Anel Memić</Text>
+					</View>
+					<View>
+						<TouchableOpacity>
+							<Image
+								source={edit}
+								style={{
+									width: 40,
+									height: 40,
+									marginTop: 40,
+									marginLeft: 80,
+									marginRight: 20,
+								}}
+							/>
+						</TouchableOpacity>
+					</View>
+				</View>
+				<View>
 					<Text
 						style={{
 							color: "white",
-							fontWeight: "bold",
-							padding: 15,
-							fontSize: 16,
+							fontSize: 30,
+							marginLeft: 20,
+							marginTop: 20,
 						}}
 					>
-						Login
+						Profile
 					</Text>
-				</TouchableOpacity>
-			</View>
+				</View>
+
+				<View style={{ alignSelf: "center", marginTop: 500 }}>
+					<TouchableOpacity onPress={() => handleLogout()}>
+						<Text style={{ color: "white" }}>Logout</Text>
+					</TouchableOpacity>
+					<TouchableOpacity
+						style={{
+							backgroundColor: "#4267B2",
+							marginTop: 30,
+							alignItems: "center",
+							height: 50,
+							width: 229,
+							borderRadius: 50,
+							alignSelf: "center",
+						}}
+						onPress={() => navigation.navigate("Login")}
+					>
+						<Text
+							style={{
+								color: "white",
+								fontWeight: "bold",
+								padding: 15,
+								fontSize: 16,
+							}}
+						>
+							Login
+						</Text>
+					</TouchableOpacity>
+				</View>
+			</ScrollView>
 		</View>
 	);
 }
