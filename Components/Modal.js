b3f8b9b49141992a2d96f7132f2d752ba7933e9f
Amar Omerika <amar.omerika@edu.fit.ba>
2022-02-02 17:52:13 +0100

    added modal component

--- Components/Modal.js
+++ Components/Modal.js
@@ -1,29 +1,27 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-import mema from "../assets/mema.png";
 
-import Modal from "./Modal";
+import levels from "../assets/levels.png";
 
-export default function Header() {
+export default function Modal() {
 	return (
 		<View
 			style={{ flexDirection: "row", backgroundColor: "#7c0000", height: 80 }}
 		>
-			<Image
-				source={mema}
-				style={{
-					height: 40,
-					width: 40,
-					marginTop: 30,
-					marginLeft: 16,
-				}}
-			/>
-			<View style={{ marginLeft: 20, marginTop: 40 }}>
-				<Text style={{ fontSize: 20, color: "white", fontWeight: "bold" }}>
-					Sessions
-				</Text>
-			</View>
-			<Modal />
+			<TouchableOpacity>
+				<View style={{ flex: 1 }}>
+					<Image
+						source={levels}
+						style={{
+							height: 40,
+							width: 40,
+							marginTop: 30,
+							marginLeft: 156,
+							marginRight: 20,
+						}}
+					/>
+				</View>
+			</TouchableOpacity>
 		</View>
 	);
 }
